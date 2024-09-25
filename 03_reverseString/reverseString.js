b1a5bc1290d326dde720e745ecd111dd6b06a3fb
uvaw <akimovivan13@gmail.com>
2024-09-25 13:54:44 +0300

    finish third

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let resultString = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        resultString += str[i];
+    }
+    return resultString;
 };
 
 // Do not edit below this line
