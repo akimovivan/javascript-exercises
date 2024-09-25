3f053b39d15c9ac3226ebb20ca67016e0bec191d
uvaw <akimovivan13@gmail.com>
2024-09-25 18:09:15 +0300

    finished fifth

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(left, right) {
+    if (left < 0 || right < 0 || !Number.isInteger(left) || !Number.isInteger(right)) {
+        return "ERROR";
+    }
+    if (left > right) {
+        let tmp = left;
+        left = right;
+        right = tmp;
+    }
 
+    let sum = 0;
+    for (let i = left; i <= right; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
