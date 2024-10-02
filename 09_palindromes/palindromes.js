76e8965d7a55ec90f337a3ba4deaa50fe2c59732
uvaw <akimovivan13@gmail.com>
2024-10-02 10:46:14 +0300

    finish ninth

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    palindrome = palindrome
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+        .replace(/\s+/g, '')
+        .toLowerCase();
+    return palindrome === palindrome.split('').reverse().join('');
 };
 
 // Do not edit below this line
