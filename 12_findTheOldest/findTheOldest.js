cd68b30a2b23128835ffcc4c7883fff532659215
uvaw <akimovivan13@gmail.com>
2024-10-02 11:41:25 +0300

    finish twelth

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear();
+    return people
+        .map(person => {
+            if (person.yearOfDeath === undefined) person.yearOfDeath = currentYear;
+            return person;
+        })
+        .sort((a, b) => {
+            return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+        })[0];
 };
 
 // Do not edit below this line
